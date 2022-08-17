65a59464af7a9cf19e31d3b4931c47dfa13303e5
Vazgen <99919037+Vazgen04@users.noreply.github.com>
2022-08-17 21:32:24 +0400

    Minor edits in Mobile navigation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,5 +1,8 @@
 const mobile = document.querySelector(".mobile");
+const body = document.querySelector(".body");
 
 function Menu() {
-    mobile.classList.toggle("active")
+    mobile.classList.toggle("active");
+    body.classList.toggle("overflow");
+
 }
